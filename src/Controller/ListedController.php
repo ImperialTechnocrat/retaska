6c364924024f9e754574572b8dc9892ca56ed993
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-05-16 19:01:01 +0200

    taska

--- src/Controller/ListedController.php
+++ src/Controller/ListedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
