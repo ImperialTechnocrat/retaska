1553ce666dfffead5fb355a8de20999a4489922f
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-05-16 18:55:49 +0200

    taska

--- src/Controller/ListedController.php
+++ src/Controller/ListedController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ListedController extends AbstractController
 {
@@ -14,6 +16,15 @@ class ListedController extends AbstractController
     {
         return $this->render('listed/index.html.twig', [
             'controller_name' => 'ListedController',
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="allproduct_show", methods={"GET"})
+     */
+    public function show(Product $product): Response
+    {
+        return $this->render('listed/show.html.twig', [
+            'product' => $product,
         ]);
     }
 }
